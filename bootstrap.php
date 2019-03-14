c2b3e847fb6dc91ead3e1323d4f501b898886820
Marcio Hass <marciohhss@gmail.com>
2019-03-14 11:41:07 -0300

    first commit

---
+++ bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use MarcioHass\CursoSON\Hello;
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('app.log', Logger::WARNING));
+
+// add records to the log
+$log->warning((new Hello)->say('Marcio'));
